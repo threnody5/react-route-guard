f26bb526745cbbc784f90ac5698806305436d233
William <williamw15@protonmail.com>
2023-01-02 00:33:26 -0500

    add login, logout functions to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 import Protected from './Component/Protected/Protected';
 import Unprotected from './Component/Unprotected/Unprotected';
+import Home from './Component/Home/Home';
 
 import './App.css';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => {
+    setIsAuthenticated(true);
+    console.log(`loggedInUser: ${isAuthenticated}`);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    console.log(`loggedInUser: ${isAuthenticated}`);
+  };
+
   return (
     <Router>
       <div>
@@ -23,6 +36,9 @@ function App() {
             <Link to='/unprotected'>Link to Unprotected Page</Link>
           </li>
         </ul>
+        <button onClick={login}>Login</button>
+        <br />
+        <button onClick={logout}>Logout</button>
       </div>
       <Routes>
         <Route
